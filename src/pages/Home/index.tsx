c95913727a370fc8459e02e7076ae6c68f032fff
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-01-23 15:34:11 -0300

    feat: consume data from api and logic from custom hook and error treatment

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,60 +1,73 @@
-import { useState } from "react"
-
-import { Header } from "../../components/Header"
-import { SearchBar } from "../../components/SearchBar"
-import { FilterSelect } from "../../components/FilterSelect"
-import { PokemonCard } from "../../components/PokemonCard"
-import { Pagination } from "../../components/Pagination"
-import { Modal } from "../../components/Modal"
-import "./home-page.scss"
-
-interface Pokemon {
-  id: number
-  name: string
-  type: string
-}
-
-const POKEMONS: Pokemon[] = [
-  { id: 1, name: "Bulbasaur", type: "grass" },
-  { id: 2, name: "Dragonair", type: "dragon" },
-  { id: 3, name: "Venusaur", type: "grass" },
-  { id: 4, name: "Raikou", type: "electric" },
-  { id: 5, name: "Blastoise", type: "water" },
-  { id: 6, name: "Mewtwo", type: "psychic" },
-  { id: 7, name: "Butterfree", type: "bug" },
-]
+import { Header } from "../../components/Header";
+import { SearchBar } from "../../components/SearchBar";
+import { FilterSelect } from "../../components/FilterSelect";
+import { PokemonCard } from "../../components/PokemonCard";
+import { Pagination } from "../../components/Pagination";
+import { Modal } from "../../components/Modal";
+import "./home-page.scss";
+import { SkeletonLoading } from "../../components/PokemonCard/SkeletonLoading";
+import { usePokemonData } from "../../hooks/usePokemonData";
 
 export function Home() {
-  const [search, setSearch] = useState("")
-  const [currentPage, setCurrentPage] = useState(2)
-  const [isOpen, setIsOpen] = useState(false)
-
-  const filteredPokemon = POKEMONS.filter((pokemon) => pokemon.name.toLowerCase().includes(search.toLowerCase()))
+  const {
+    search,
+    setSearch,
+    currentPage,
+    setCurrentPage,
+    totalPages,
+    currentPageData,
+    isFetching,
+    error,
+    totalFilteredPokemons,
+  } = usePokemonData({ pageSize: 30 });
 
   return (
     <>
-        <Header />
-        <main className="pokemon-grid">
-            <div className="pokemon-grid__controls">
-                <SearchBar value={search} onChange={setSearch} />
-                <FilterSelect />
-            </div>
+      <Header />
+      <main className="pokemon-grid">
+        <div className="pokemon-grid__controls">
+          <SearchBar value={search} onChange={setSearch} />
+          <FilterSelect />
+        </div>
 
-            <div className="pokemon-grid__total">
-                <img src="../../assets/pokemon-icon.svg" alt="" />
-                Total: {filteredPokemon.length} Pokémons
-            </div>
+        <div className="pokemon-grid__total">
+          <img src="../../assets/pokemon-icon.svg" alt="" />
+          Total: {totalFilteredPokemons} Pokémons
+        </div>
 
-            <div className="pokemon-grid__cards">
-                {filteredPokemon.map((pokemon) => (
-                <PokemonCard key={pokemon.id} name={pokemon.name} type={pokemon.type} onClick={() => setIsOpen(true)} />
-                ))}
-            </div>
-            <Modal isOpen={isOpen} toggleModal={() => setIsOpen(!isOpen)} pokemonName={'Name'}/>
+        <div className="pokemon-grid__cards">
+          {isFetching ? (
+            Array.from({ length: 12 }).map((_, id) => <SkeletonLoading key={id} />)
+          ) : (
+            currentPageData.length > 0 ? (
+              currentPageData.map((pokemon) => (
+                <PokemonCard
+                  key={pokemon.id}
+                  name={pokemon.name}
+                  type={pokemon.types[0]}
+                  imageUrl={pokemon.images.small}
+                  rarity={pokemon.rarity}
+                  symbol={pokemon.set?.images?.symbol}
+                  id={pokemon.id}
+                />
+              ))
+            ) : (
+              <div className="error-message-wrapper">
+                <span className="error-message">Nenhum Pokemon encontrado.</span>
+              </div>
+            )
+          )}
+        </div>
+        {error && (
+          <div className="error-message-wrapper">
+            <span className="error-message">Erro. Nenhum dado encontrado.</span>
+          </div>
+        )}
 
-            <Pagination currentPage={currentPage} onPageChange={setCurrentPage} />
-        </main>
+        <Modal />
+
+        <Pagination currentPage={currentPage} onPageChange={setCurrentPage} totalPages={totalPages} />
+      </main>
     </>
-  )
+  );
 }
-
