389351bb7a7a84abe10db41b32e2adc21492aa3d
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-01-23 20:19:54 -0300

    feat: add dynamic route to pokemon page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Pokemon } from "./pages/Pokemon";
 import './app.scss'
 
 
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/pokemon/:id" element={<Pokemon />} />
       </Routes>
     </Router>
   );
