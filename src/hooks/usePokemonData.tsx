9e68d9289b84d98b7151ec29ff609b48c319d3c4
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-01-23 15:32:08 -0300

    feat: create custom hook use-pokemon-data with the logic to home page

---
+++ src/hooks/usePokemonData.tsx
@@ -0,0 +1,56 @@
+import { useState, useMemo } from "react";
+import { useGetPokemons } from "./useGetPokemons";
+import useFilterStore from "../stores/FilterStore";
+
+interface UsePokemonDataProps {
+  pageSize: number;
+}
+
+export function usePokemonData({ pageSize }: UsePokemonDataProps) {
+    
+  const { data: pokemonsData, error, isFetching } = useGetPokemons();
+  const { filteredBy } = useFilterStore();
+
+  const [search, setSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const filteredPokemons = useMemo(() => {
+    if (!pokemonsData) return [];
+
+    // Filter by search
+    let filtered = pokemonsData.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(search.toLowerCase()),
+      setCurrentPage(1)
+    );
+
+    // Filter by selected filter
+    if (filteredBy !== "all") {
+      filtered = filtered.filter((pokemon) => {
+        if (filteredBy === "hpLessThan100") return Number(pokemon.hp) < 100;
+        if (filteredBy === "hpGreaterThan100") return Number(pokemon.hp) > 100;
+        return true;
+      });
+    }
+
+    return filtered;
+  }, [search, filteredBy, pokemonsData]);
+
+  const totalPages = Math.ceil(filteredPokemons.length / pageSize);
+
+  const currentPageData = useMemo(() => {
+    const startIndex = (currentPage - 1) * pageSize;
+    return filteredPokemons.slice(startIndex, startIndex + pageSize);
+  }, [filteredPokemons, pageSize, currentPage]);
+
+  return {
+    search,
+    setSearch,
+    currentPage,
+    setCurrentPage,
+    totalPages,
+    currentPageData,
+    isFetching,
+    error,
+    totalFilteredPokemons: filteredPokemons.length,
+  };
+}
