e4e4d34fb0c4a3a41e887b795cde8dbc2c0b9a23
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-01-23 15:36:44 -0300

    feat: add handle functions and logic to build and display pagination format

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,18 +1,34 @@
 import useMedia from "use-media";
 import "./pagination-styles.scss"
+import { getPageNumbers } from "../../utils/functions/getPageNumbers";
 
 interface PaginationProps {
   currentPage: number
   onPageChange: (page: number) => void
+  totalPages: number
 }
 
-export function Pagination({ currentPage, onPageChange }: PaginationProps) {
+export function Pagination({ currentPage, onPageChange, totalPages }: PaginationProps) {
 
     const isMobile = useMedia({maxWidth: '719px'});
 
+    const handlePrevious = () => {
+        if (currentPage > 1) {
+            onPageChange(currentPage - 1);
+        }
+    };
+
+    const handleNext = () => {
+        if (currentPage < totalPages) {
+            onPageChange(currentPage + 1);
+        }
+    };
+    
+    const pageNumbers = getPageNumbers(currentPage, totalPages);
+
     return (
         <div className="pagination">
-        <button className="pagination__button" onClick={() => onPageChange(currentPage - 1)}>
+        <button className="pagination__button" onClick={handlePrevious} disabled={currentPage === 1}>
             <img src="../../assets/left-arrow.svg" alt="" />
             Anterior
         </button>
@@ -22,16 +38,17 @@ export function Pagination({ currentPage, onPageChange }: PaginationProps) {
             )
         }
         { 
-            !isMobile && [1, 2, 3, 4, 5].map((page) => (
+            !isMobile && pageNumbers.map((page) => (
                 <button
                     key={page}
                     className={`pagination__button ${page === currentPage ? "pagination__button--active" : ""}`}
-                    onClick={() => onPageChange(page)}
+                    onClick={() => typeof page === "number" && onPageChange(page)}
+                    disabled={page === currentPage || typeof page !== "number"}
                 >
                     {page}
                 </button>
         ))}
-        <button className="pagination__button" onClick={() => onPageChange(currentPage + 1)}>
+        <button className="pagination__button" onClick={handleNext} disabled={currentPage === totalPages}>
             Próximo
             <img src="../../assets/right-arrow.svg" alt="" />
         </button>
