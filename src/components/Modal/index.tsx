2b5e940df5c52cfeae30a93bf0e1ee7b62d9403c
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-01-23 11:14:13 -0300

    feat: add pokemon info from selected pokemon

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,54 +2,62 @@ import { X } from 'lucide-react'
 import "./modal-styles.scss"
 import { InfoCard } from '../InfoCard'
 import useMedia from 'use-media'
+import { useGetPokemons } from '../../hooks/useGetPokemons'
+import useModalStore from '../../stores/ModalStore'
+import { PokemonCardType } from '../../utils/types'
 
-interface ModalProps {
-  isOpen: boolean
-  toggleModal: () => void
-  pokemonName: string
-}
 
-export function Modal({ isOpen, toggleModal, pokemonName }: ModalProps) {
+export function Modal() {
     
+    const { data: pokemonsData } = useGetPokemons();
+    const { setIsModalOpen, isModalOpen, pokemonSelectedId } = useModalStore();
     const isMobile = useMedia({ maxWidth: '719px' });
 
-    if (!isOpen) return null
+    if (!isModalOpen) return null
+
+    const selectedPokemon: PokemonCardType = pokemonsData?.find(pokemon => pokemon.id === pokemonSelectedId) as PokemonCardType;
+
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen)
+    }
 
     return (
         <div className="modal-overlay" onClick={toggleModal}>
         <div className="modal">
             <div className="modal__header">
-            <h2 className="modal__title">{pokemonName}</h2>
+            <h2 className="modal__title">{selectedPokemon?.name}</h2>
             <button className="modal__close" onClick={toggleModal}>
                 <X />
             </button>
             </div>
 
             <div className="modal__content">
-            <div className="modal__image">
-                <div className="modal__placeholder" />
+            <div className="modal__image-wrapper">
+                <img 
+                    className="pokemon-card__image"
+                    src={selectedPokemon?.images.small} 
+                    alt="Pokemon Image" 
+                />
             </div>
 
             <div className="modal__tags">
-                <span className="modal__tag">Lorem ipsum</span>
-                <span className="modal__tag">dolor amet</span>
+                <span className="modal__tag1">HP {selectedPokemon?.hp}</span>
+                <span className="modal__tag2">{selectedPokemon?.types[0]}</span>
             </div>
 
             {
                 !isMobile && (
                     <div className="modal__info-grid">
-                        <InfoCard text="lorem ipsum" />
-                        <InfoCard text="lorem ipsum" />
-                        <InfoCard text="lorem ipsum" />
+                        <InfoCard title={"HP"} info={selectedPokemon?.hp} />
+                        <InfoCard title={"Evolui de"} info={selectedPokemon?.evolvesFrom || "- - -"} />
+                        <InfoCard title={"Evolui para"} info={selectedPokemon?.evolvesTo || "- - -"} />
                     </div>
                 )
             }
             
 
             <p className="modal__description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-                ea commodo consequat
+                {selectedPokemon?.flavorText}
             </p>
 
             <button className="modal__button">
