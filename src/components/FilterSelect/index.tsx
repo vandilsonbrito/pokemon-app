1c236045d8daf12d115a250ddda7d5396265e490
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-01-23 11:08:03 -0300

    refactor: change input select values

--- src/components/FilterSelect/index.tsx
+++ src/components/FilterSelect/index.tsx
@@ -1,15 +1,25 @@
+import useFilterStore from "../../stores/FilterStore"
 import "./filter-select-styles.scss"
 
 export function FilterSelect() {
-  return (
-    <div className="filter-select">
-        <img src="../../assets/filter-vector.svg" alt="" className="filter-select__icon"/>
-        <span className="filter-select__label">Filtrar por:</span>
-        <select className="filter-select__input">
-            <option value="all" disabled selected>Todos</option>
-            <option value="type">Tipo</option>
-            <option value="name">Nome</option>
-        </select>
-    </div>
-  )
+
+    const { setFilteredBy } = useFilterStore();
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setFilteredBy(event.target.value)
+    }
+
+    return (
+        <div className="filter-select">
+            <img src="../../assets/filter-vector.svg" alt="" className="filter-select__icon"/>
+            <span className="filter-select__label">Filtrar por:</span>
+            <select
+                onChange={handleChange} 
+                className="filter-select__input">
+                <option value="all" selected>Todos</option>
+                <option value="hpLessThan100">HP Menor que 100</option>
+                <option value="hpGreaterThan100">HP Maior que 100</option>
+            </select>
+        </div>
+    )
 }
